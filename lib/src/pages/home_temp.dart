98cf2147267bbd34c2bbb2ead6c3033cd3a32caa
Alexander Londoño Espejo <alexlondon07@gmail.com>
2020-06-18 15:23:09 -0500

    Listview - Mostrar una lista de elementos

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+class HomePageTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Components temp'),
+      ),
+      body:  ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('Hello'), 
+          ),
+          ListTile(
+            title: Text('Hello'), 
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Hello'), 
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Hello'), 
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
