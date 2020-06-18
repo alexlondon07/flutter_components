5145bae3be117275d700bac327d16b7990612cca
Alexander Londoño Espejo <alexlondon07@gmail.com>
2020-06-18 15:44:02 -0500

    Crear ListTiles a partir de una lista estática

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 
 class HomePageTemp extends StatelessWidget {
+
+  final options = ['Uno', 'Dos', 'Tres', 'Cuatro'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,24 +11,23 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Components temp'),
       ),
       body:  ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('Hello'), 
-          ),
-          ListTile(
-            title: Text('Hello'), 
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Hello'), 
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Hello'), 
-          ),
-          Divider(),
-        ],
+        children: _createItems(),
       ),
     );
+  }
+
+  List<Widget> _createItems(){
+
+    List<Widget> list = new List<Widget>();
+
+    for(String opt in options) {
+    
+      final tempWidget = ListTile(
+        title: Text (opt),
+      );
+      list..add( tempWidget )
+          ..add( Divider());
+    }
+    return list;
   }
 }
