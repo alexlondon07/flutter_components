56ad5d284aaf550fe4598173020e77053fe825d0
Alexander Londoño Espejo <alexlondon07@gmail.com>
2020-07-12 13:36:30 -0500

    Navegar con rutas con nombre

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/alert_page.dart';
+import 'package:flutter_components/src/pages/avatar_page.dart';
 import 'package:flutter_components/src/pages/home_page.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Components App',
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      // home: HomePage(),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => HomePage(),
+        'avatar': (BuildContext context) => AvatarPage(),
+        'alert': (BuildContext context) => AlertPage(),
+      },
     );
   }
 }
