8435b808ad66045f134ac5ef576cceedc0d8138a
Alexander Londoño Espejo <alexlondon07@gmail.com>
2020-07-19 11:00:35 -0500

    73. Image y FadeIn Image

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,7 +9,7 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(10.0),
-        children: <Widget>[_cardType1()],
+        children: <Widget>[_cardType1(), SizedBox(height: 30.0), _cardType2()],
       ),
     );
   }
@@ -29,6 +29,30 @@ class CardPage extends StatelessWidget {
               FlatButton(onPressed: () {}, child: Text('Cancelar')),
               FlatButton(onPressed: () {}, child: Text('Ok'))
             ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardType2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'),
+            placeholder: AssetImage('assets/loading.gif'),
+            fadeInDuration: Duration(milliseconds: 100),
+            height: 250.0,
+            fit: BoxFit.cover,
+          ),
+          // Image(
+          //     image: NetworkImage(
+          //         'https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Descripción de la imagen'),
           )
         ],
       ),
