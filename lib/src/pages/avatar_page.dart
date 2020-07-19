06a9fb760cc70d33969b961e911382aefca0ee33
Alexander Londoño Espejo <alexlondon07@gmail.com>
2020-07-19 17:26:02 -0500

    76. Widget - Alert

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://avatars0.githubusercontent.com/u/5550470?s=460&u=4777f57a419f225831521ed9b75091214e1c9fdc&v=4'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+            fadeInDuration: Duration(milliseconds: 200),
+            placeholder: AssetImage('assets/loading.gif'),
+            image: NetworkImage(
+                'https://avatars0.githubusercontent.com/u/5550470?s=460&u=4777f57a419f225831521ed9b75091214e1c9fdc&v=4')),
       ),
     );
   }
