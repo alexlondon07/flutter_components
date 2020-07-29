ded5de391d88d42feab6a6a46cada4ef653caddc
Alexander Londoño Espejo <alexlondon07@gmail.com>
2020-07-29 11:52:40 -0500

    AnimatedContainer

--- lib/src/pages/animated_container.dart
+++ lib/src/pages/animated_container.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AnimatedContainerPage extends StatefulWidget {
@@ -8,8 +10,50 @@ class AnimatedContainerPage extends StatefulWidget {
 }
 
 class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+
+  double _width = 50.0;
+  double _heigth = 50.0;
+  Color _color = Colors.pink;
+  BorderRadiusGeometry _borderRadiusGeometry = BorderRadius.circular(8.0);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animated Container')
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: Duration(
+            seconds: 1
+          ),
+          curve: Curves.fastOutSlowIn,
+          width: _width,
+          height: _heigth,
+          decoration: BoxDecoration(
+            borderRadius:  _borderRadiusGeometry,
+            color: _color
+          ),
+      ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.offline_bolt),
+        onPressed: _change,
+      ),
+    );
+  }
+
+  void _change(){
+    final random = Random();
+    setState(() {
+      _width = random.nextInt(300).toDouble();
+      _heigth = random.nextInt(300).toDouble();
+      _color = Color.fromRGBO(
+        random.nextInt(255),
+        random.nextInt(255),
+        random.nextInt(255),
+        1 );
+        _borderRadiusGeometry = BorderRadius.circular( random.nextInt(100).toDouble() );
+    });
   }
 }
