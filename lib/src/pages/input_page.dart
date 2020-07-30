335b439d01689ce4226097b0ab719a5a7f2626c3
Alexander Londoño Espejo <alexlondon07@gmail.com>
2020-07-30 14:46:16 -0500

    Inputs

---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+
+  String _name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs')
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+        children: <Widget>[
+          _createInput(),
+          Divider(),
+          _createPerson(),
+        ]
+      ),
+    );
+  }
+
+  Widget _createInput(){
+    return TextField(
+      autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        counter: Text('Letras ${ _name.length }'),
+        hintText: 'Nombre de la persona',
+        labelText: 'Nombre',
+        helperText: 'Solo es el nombre',
+        suffixIcon:  Icon ( Icons.accessibility),
+        icon: Icon (Icons.account_circle),
+      ),
+      onChanged: (value){
+        setState(() {
+          _name = value;
+        });
+      },
+    );
+  }
+
+  Widget _createPerson(){
+    return ListTile(
+      title: Text('Nombre es: $_name'),
+    );
+  }
+}
